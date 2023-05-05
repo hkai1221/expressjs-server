5bd05c4480a0620323e8e36220d1054ee9723e36
hkai1221 <1119654491@qq.com>
2023-05-05 20:42:42 +0800

    添加番号查询

--- index.js
+++ index.js
@@ -35,7 +35,14 @@ try {
     const keyword = ctx.message.text;
     const url = `https://javdb.com/search?q=${keyword}`;
     axios
-      .get(url)
+      .get(url,{
+        headers: {
+          "User-Agent":
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 " +
+            "(KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+        },
+        
+      })
       .then((response) => {
         const html = response.data;
         const $ = cheerio.load(html);
